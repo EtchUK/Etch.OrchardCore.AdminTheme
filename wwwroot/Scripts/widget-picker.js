c962c9fbbae9af125ebd22d4a55577ed2beab5dd
Peter Keating <pete.keating@etchuk.com>
2020-05-15 12:24:24 +0100

    Add flow editor improvements
    Override `FlowPart` edit template to improve UX when adding widgets.
    Uses a dialog (replicating adding activities to a Workflow) where
    widgets can be filtered by a category or searched with a term.

---
+++ wwwroot/Scripts/widget-picker.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    var $categoryPickers = document.querySelectorAll('.widget-picker-categories .nav-item');
+
+    var applyFilter = function (category, term) {
+        var $widgets = document.querySelectorAll('.widget');
+
+        category = category || document.querySelectorAll('.widget-picker-categories .nav-link.active')[0].getAttribute('href').substr(1);
+        term = term || document.querySelector('.modal-widgets input[type=search]').value;
+
+        $widgets.forEach(function ($widget) {
+            if (category !== 'all' && $widget.getAttribute('data-category') !== category) {
+                $widget.classList.add('d-none');
+                return;
+            }
+
+            if (term && $widget.innerHTML.toLowerCase().indexOf(term.toLowerCase()) < 0) {
+                $widget.classList.add('d-none');
+                return;
+            }
+
+            $widget.classList.remove('d-none');
+        });
+    };
+
+    var addCategorySelectListeners = function ($el) {
+        $el.addEventListener('click', function (e) {
+            applyFilter(e.target.getAttribute('href').substring(1));
+            return false;
+        });
+    };
+
+    $categoryPickers.forEach(addCategorySelectListeners);
+
+    document.querySelector('.modal-widgets input[type=search]').addEventListener('keyup', function (e) {
+        applyFilter(null, e.target.value);
+    });
+})();

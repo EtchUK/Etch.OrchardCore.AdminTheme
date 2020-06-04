45d49f04f698d0e393183a785665e511efd5576c
Peter Keating <pete.keating@etchuk.com>
2020-06-04 11:37:47 +0100

    Bump v0.2.2

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Admin Theme",
     Author = "EtchUK Ltd.",
     Website = "https://etchuk.com/",
-    Version = "0.2.1",
+    Version = "0.2.2",
     Description = "Extension of TheAdmin theme.",
     Tags = new[] { "admin" },
     BaseTheme = "TheAdmin"

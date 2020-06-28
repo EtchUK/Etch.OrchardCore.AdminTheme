7cc0c2dde332f86b098ea25c8a8cf56c9f5e6776
Peter Keating <pete.keating@etchuk.com>
2020-06-28 22:11:16 +0100

    Upgrade to RC2
    - Update target framework to 3.1
    - Update Orchard Core references to RC2
    - Update travis build to target 3.1
    - Remove existing fixes to admin theme for RC1
    - Ensure custom flow editor is compatible with RC2
    - Update version to 0.3.0

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Admin Theme",
     Author = "EtchUK Ltd.",
     Website = "https://etchuk.com/",
-    Version = "0.2.2",
+    Version = "0.3.0",
     Description = "Extension of TheAdmin theme.",
     Tags = new[] { "admin" },
     BaseTheme = "TheAdmin"
--- wwwroot/Scripts/widget-picker.js
+++ wwwroot/Scripts/widget-picker.js
@@ -1,37 +1,54 @@
 ﻿(function () {
-    var $categoryPickers = document.querySelectorAll('.widget-picker-categories .nav-item');
+    window.initialisedPickers = [];
 
-    var applyFilter = function (category, term) {
-        var $widgets = document.querySelectorAll('.js-flow-widget');
+    window.displayPicker = function ($picker) {
+        if (window.initialisedPickers.indexOf($picker) >= 0) {
+            return;
+        }
 
-        category = category || document.querySelectorAll('.widget-picker-categories .nav-link.active')[0].getAttribute('href').substr(1);
-        term = term || document.querySelector('.modal-widgets input[type=search]').value;
+        window.initialisedPickers.push($picker);
 
-        $widgets.forEach(function ($widget) {
-            if (category !== 'all' && $widget.getAttribute('data-category') !== category) {
-                $widget.classList.add('d-none');
-                return;
-            }
+        var $categoryPickers = $picker.querySelectorAll('.widget-picker-categories .nav-item');
 
-            if (term && $widget.innerHTML.toLowerCase().indexOf(term.toLowerCase()) < 0) {
-                $widget.classList.add('d-none');
-                return;
-            }
+        var applyFilter = function (category, term) {
+            var $widgets = document.querySelectorAll('.js-flow-widget')
 
-            $widget.classList.remove('d-none');
-        });
+            category = category || document.querySelectorAll('.widget-picker-categories .nav-link.active')[0].getAttribute('href').substr(1);
+            term = term || document.querySelector('.modal-widgets input[type=search]').value;
+
+            $widgets.forEach(function ($widget) {
+                if (category !== 'all' && $widget.getAttribute('data-category') !== category) {
+                    $widget.classList.add('d-none');
+                    return;
+                }
+
+                if (term && $widget.innerHTML.toLowerCase().indexOf(term.toLowerCase()) < 0) {
+                    $widget.classList.add('d-none');
+                    return;
+                }
+
+                $widget.classList.remove('d-none');
+            });
+        };
+
+        var addCategorySelectListeners = function ($el) {
+            $el.addEventListener('click', function (e) {
+                applyFilter(e.target.getAttribute('href').substring(1));
+                return false;
+            });
+        };
+
+        $categoryPickers.forEach(addCategorySelectListeners);
+
+        var $widgetSearch = $picker.querySelector('input[type=search]');
+
+        if ($widgetSearch) {
+            var onSearch = function (e) {
+                applyFilter(null, e.target.value);
+            };
+
+            $widgetSearch.addEventListener('keyup', onSearch);
+            $widgetSearch.addEventListener('search', onSearch);
+        }
     };
-
-    var addCategorySelectListeners = function ($el) {
-        $el.addEventListener('click', function (e) {
-            applyFilter(e.target.getAttribute('href').substring(1));
-            return false;
-        });
-    };
-
-    $categoryPickers.forEach(addCategorySelectListeners);
-
-    document.querySelector('.modal-widgets input[type=search]').addEventListener('keyup', function (e) {
-        applyFilter(null, e.target.value);
-    });
 })();

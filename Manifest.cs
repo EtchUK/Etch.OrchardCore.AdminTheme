7b6cbdae6b35daf14746274983ff0ef28cebb1ee
Peter Keating <pete.keating@etchuk.com>
2021-03-09 21:42:30 +0000

    v0.5.1

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Admin Theme",
     Author = "EtchUK Ltd.",
     Website = "https://etchuk.com/",
-    Version = "0.5.0",
+    Version = "0.5.1",
     Description = "Extension of TheAdmin theme.",
     Tags = new[] { "admin" },
     BaseTheme = "TheAdmin"

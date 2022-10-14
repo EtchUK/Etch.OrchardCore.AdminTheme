5e77043083c90b57412fe837b7c9881dee86f309
Peter Keating <pete.keating@etchuk.com>
2022-10-14 17:14:53 +0100

    Upgrade Orchard Core to 1.4

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Admin Theme",
     Author = "EtchUK Ltd.",
     Website = "https://etchuk.com/",
-    Version = "1.3.1",
+    Version = "1.4.0",
     Description = "Extension of TheAdmin theme.",
     Tags = new[] { "admin" },
     BaseTheme = "TheAdmin"

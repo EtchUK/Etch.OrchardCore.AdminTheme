290edfb905e4e9da4c447d26369c6600ccfb0e8b
Peter Keating <pete.keating@etchuk.com>
2023-03-11 09:16:18 +0000

    Use git tag name to populate version number during deployment

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Admin Theme",
     Author = "EtchUK Ltd.",
     Website = "https://etchuk.com/",
-    Version = "1.4.2",
+    Version = "$(VersionNumber)",
     Description = "Extension of TheAdmin theme.",
     Tags = new[] { "admin" },
     BaseTheme = "TheAdmin"
